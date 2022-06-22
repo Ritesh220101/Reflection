db114dceb766ef8481d9351ab1f6d3a09eaae4ba
Ritesh220101 <68066271+Ritesh220101@users.noreply.github.com>
2022-06-22 18:33:26 +0530

    Update Reflection.java

--- Reflection.java
+++ Reflection.java
@@ -6,7 +6,7 @@ public class Reflection {
 	public void methods(Class className) {
 		Method[] methods = className.getDeclaredMethods();
 		for (Method method : methods) {
-			System.out.println(method.getName());
+			System.out.println(method);
 		}
 	}
 	public void fields(Class className) {
